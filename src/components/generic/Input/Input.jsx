a68ea0acd15248c1c916ff4eadeecfee0cc5ae38
Aleksandar <aca96@live.co.uk>
2023-01-17 13:17:52 +0100

    Fixed styles for Input

--- src/components/generic/Input/Input.jsx
+++ src/components/generic/Input/Input.jsx
@@ -2,19 +2,14 @@ import React from 'react';
 
 export function Input() {
 	return (
-		<div>
-			<label
-				htmlFor="first_name"
-				className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-			>
-				First name
-				<input
-					type="text"
-					id="first_name"
-					className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-					placeholder="John"
-				/>
-			</label>
+		<div className="flex w-[500px]">
+            <span className='text-sm'>First Name</span>
+			<input
+				type="text"
+				id="first_name"
+				className="outline-primary placeholder-secondary-200 bg-gray-50 border border-primary text-secondary-300 text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-primary dark:border-primary dark:placeholder-secondary-200 dark:text-secondary-300 dark:focus:ring-primary dark:focus:border-primary"
+				placeholder="John"
+			/>
 		</div>
 	);
 }
